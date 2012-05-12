2a812a848dfc2c9daaa23080a69118a2348f68c4
Dan Printzell <wildn00b@softwepz.co.cc>
2012-05-12 18:48:02 +0200

    Added so it can load TerraraServer.exe and run it, and maked it 200% cooler by change text color to green.

--- Tewwawia/Program.cs
+++ Tewwawia/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Reflection;
+using System.IO;
 
 namespace Tewwawia
 {
@@ -9,6 +8,40 @@ namespace Tewwawia
     {
         static void Main(string[] args)
         {
+            int ret = new Program().Run(args);
+#if DEBUG
+            Console.WriteLine((ret == 0) ? "EVERYTHING IS OKEY!!!" : "THE WORLD IS GOING TO END!!! ret: " + ret);
+            Console.ReadLine();
+#endif
+        }
+
+        public TextWriter errorWriter = Console.Error;
+        public Assembly TerrariaServer;
+
+        private int Run(string[] args)
+        {
+            if (!Load())
+                return -1;
+            Console.ForegroundColor = ConsoleColor.Green; // Make terraria 200% cooler
+            TerrariaServer.EntryPoint.Invoke(null, new object[] { args });
+
+            return 0;
+        }
+
+        private bool Load()
+        {
+            try
+            {
+                if (!File.Exists(Environment.CurrentDirectory + "/TerrariaServer.exe"))
+                    throw new FileNotFoundException("'TerrariaServer.exe' not found, need to be in the same folder as Tewwawia.exe!");
+                TerrariaServer = Assembly.LoadFile(Environment.CurrentDirectory + "/TerrariaServer.exe");
+            }
+            catch (Exception e)
+            {
+                errorWriter.WriteLine(e.Message);
+                return false;
+            }
+            return true;
         }
     }
 }
